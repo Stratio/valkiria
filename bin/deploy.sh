d44ffc47a08131b5255d7dbae915cdc4c2efa7e2
antonioalf <aalfonso@stratio.com>
2016-11-30 08:39:39 +0100

    add jenkinsfile

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -e
 
+BASEDIR=`dirname $0`/..
+VERSION=`cat $BASEDIR/VERSION`
+# Upload normal universe
+cd target/bin && curl -sS -u stratio:${NEXUSPASS} --upload-file valkiria-${VERSION}.tar.gz http://sodio.stratio.com/nexus/content/sites/paas/valkiria/${VERSION}/
