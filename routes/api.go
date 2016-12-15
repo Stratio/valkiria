1e696227bed3084234ad99748baee5f040e4ebbb
jpgilaberte <jpgilaberte@stratio.com>
2016-12-15 16:50:43 +0100

    Refactor valkiria rest api

--- routes/api.go
+++ routes/api.go
@@ -6,75 +6,69 @@ import (
 	"github.com/Stratio/valkiria/proc"
 	"golang.org/x/net/context"
 	"net/http"
-	"strconv"
 	"time"
+	"strings"
 )
 
 type response struct {
-	Status string `json:"status,omitempty"`
 	Code   string `json:"code,omitempty"`
+	Status string `json:"status,omitempty"`
+	Proc   proc.Process `json:"process,omitempty"`
 }
 
 type responseError struct {
+	Code   string `json:"code,omitempty"`
 	Status string `json:"status,omitempty"`
 	Cause  string `json:"cause,omitempty"`
-	Code   string `json:"code,omitempty"`
 }
 
-func handleChaos(ctx context.Context, w http.ResponseWriter, r *http.Request) *HttpError {
+type ListRequest struct {
+	Code string 		`json:"code,omitempty"`
+	Daemon []proc.Daemon	`json:"daemon,omitempty"`
+	Docker []proc.Docker	`json:"docker,omitempty"`
+	Service []proc.Service	`json:"service,omitempty"`
+}
 
-	log.Debug("routes.api.handleDaemon")
-
-	daemon, err := strconv.Atoi(r.URL.Query()["daemon"][0])
-	service, err := strconv.Atoi(r.URL.Query()["service"][0])
-	docker, err := strconv.Atoi(r.URL.Query()["docker"][0])
-
-	if err == nil {
-		proc := proc.Manager{}
-		proc.ConfigManager()
-		proc.LoadProcesses()
-		err = proc.Chaos(daemon, service, docker)
-		if err != nil {
-			log.Debug("routes.api.handleDaemon - ERROR:" + err.Error())
-			json.NewEncoder(w).Encode(response{Status: "error: " + err.Error()})
-		} else {
-
-			json.NewEncoder(w).Encode(response{Code: "200", Status: "succes"})
-		}
-	} else {
-		json.NewEncoder(w).Encode(response{Code: "200", Status: "session locked"})
-	}
-	return nil
+type ShooterRequest struct {
+	Name string
+	ServiceType int
+	KillExecutor bool
 }
 
 func handleShooter(ctx context.Context, w http.ResponseWriter, r *http.Request) *HttpError {
 	var timeStart = time.Now()
 	log.Debugf("routes.api.handleShooter - START '%v'", timeStart)
-	//01. Validate params
-	name := r.URL.Query().Get("name")
-	typeService, err := strconv.Atoi(r.URL.Query().Get("typeService"))
-	killExecutor, err := strconv.ParseBool(r.URL.Query().Get("killExecutor"))
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var t ShooterRequest
+	err := decoder.Decode(&t)
 	if err != nil {
-		log.Infof("routes.api.handleShooter - ERROR: '%v'", err.Error())
-		json.NewEncoder(w).Encode(responseError{Cause: "Invalid params.", Code: "400", Status: "Error: " + err.Error()})
-	} else {
-		//02. Load process
-		var p = proc.Manager{}
-		p.ConfigManager()
-		p.LoadProcesses()
-		b, err := p.Shooter(name, typeService, killExecutor)
-		if b {
-			json.NewEncoder(w).Encode(response{Code: "200", Status: "Succes"})
-		} else {
-			if err == nil {
-				log.Info("routes.api.handleShooter - WARNNING: 'Service not found'")
-				json.NewEncoder(w).Encode(response{Code: "404", Status: "Service not found"})
-			} else {
-				log.Infof("routes.api.handleShooter - ERROR: '%v'", err.Error())
-				json.NewEncoder(w).Encode(responseError{Cause: "Server error.", Code: "500", Status: "Error: " + err.Error()})
-			}
-		}
+		log.Warnf("routes.api.handleShooter - '%v'", err.Error())
+		json.NewEncoder(w).Encode(responseError{Code: "400", Status: "Invalid params", Cause: err.Error()})
+		return nil
 	}
+	if strings.EqualFold(t.Name, "") {
+		log.Warnf("routes.api.handleShooter - Name field is mandatory and should not be empty")
+		json.NewEncoder(w).Encode(responseError{Code: "400", Status: "Invalid params", Cause: "Name field is mandatory and should not be empty"})
+		return nil
+	}
+
+	var p = proc.Manager{}
+	p.ConfigManager()
+	p.LoadProcesses()
+	proc, err := p.Shooter(t.Name, t.ServiceType, t.KillExecutor)
+	switch {
+		case proc != nil:
+			json.NewEncoder(w).Encode(response{Code: "200", Status: "Succes", Proc: proc})
+		case err == nil:
+			log.Warning("routes.api.handleShooter - 'TASK NOT FOUND'")
+			json.NewEncoder(w).Encode(responseError{Code: "404", Status: "Empty result", Cause: "TASK NOT FOUND"})
+		default:
+			log.Errorf("routes.api.handleShooter - '%v'", err.Error())
+			json.NewEncoder(w).Encode(responseError{Code: "500", Status: "Server error", Cause: err.Error()})
+	}
+
 	log.Debugf("routes.api.handleShooter - FINISH - '%v'", time.Since(timeStart))
 	return nil
 }
@@ -82,28 +76,17 @@ func handleShooter(ctx context.Context, w http.ResponseWriter, r *http.Request) 
 func handleList(ctx context.Context, w http.ResponseWriter, r *http.Request) *HttpError {
 	var timeStart = time.Now()
 	log.Debugf("routes.api.handleList - START - '%v'", timeStart)
-	//01. Validate params
 
-	//02. Load process
 	var p = proc.Manager{}
 	p.ConfigManager()
-	err := p.LoadProcesses()
 
-	//03. Paint processes in log
-	if err == nil {
-		for _, d := range p.Daemons {
-			log.Infof("routes.api.handleList daemon - '%v' '%v' '%v' -", d.Name, d.Pid, d.Path)
-		}
-		for _, s := range p.Services {
-			log.Infof("routes.api.handleList service - '%v' '%v' '%v' '%v' '%v' -", s.TaskName, s.Pid, s.Name, s.Executor, s.Ppid)
-		}
-		for _, do := range p.Dockers {
-			log.Infof("routes.api.handleList docker - '%v' '%v' '%v' -", do.TaskName, do.Image, do.Name)
-		}
-		json.NewEncoder(w).Encode(response{Code: "200", Status: "succes"})
-	} else {
-		json.NewEncoder(w).Encode(responseError{Cause: "Server error.", Code: "500", Status: "Error: " + err.Error()})
+	var err error
+	if err = p.LoadProcesses(); err == nil{
+		json.NewEncoder(w).Encode(ListRequest{Code: "200", Daemon: p.Daemons, Docker: p.Dockers, Service: p.Services})
+		return nil
 	}
+
+	json.NewEncoder(w).Encode(responseError{Code: "500", Status: "Server error", Cause: err.Error()})
 	log.Debugf("routes.api.handleList - FINISH - '%v'", time.Since(timeStart))
 	return nil
 }
--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,14 @@ package routes
 
 var RoutesAgent = map[string]map[string]Handler{
 	"GET": {
-		"/api/v1/chaos":   handleChaos,
-		"/api/v1/shooter": handleShooter,
 		"/api/v1/list":    handleList,
+	},
+	"POST": {
+		"/api/v1/valkiria": handleShooter,
 	},
 }
 
 var RoutesOrchestrator = map[string]map[string]Handler{
 	"GET": {
-		"/api/v1/chaos":   handleChaos,
-		"/api/v1/shooter": handleShooter,
-		"/api/v1/list":    handleList,
 	},
 }
