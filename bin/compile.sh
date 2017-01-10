4d8449f90952ba808fbc79d909e30127e6429970
adoblas <adoblas@stratio.com>
2017-01-10 16:30:55 +0100

    Fix error when compile twice

--- bin/compile.sh
+++ bin/compile.sh
@@ -11,14 +11,13 @@ mkdir -p $GOPATH/bin $GOPATH/pkg $GOPATH/src/github.com/Stratio/valkiria
 go get -u github.com/tools/godep
 go get -u github.com/mattn/goveralls
 go get -u github.com/jstemmer/go-junit-report
-if [$PWD != $GOPATH];then
-	ln -s $PWD/valkiria $GOPATH/src/github.com/Stratio/valkiria/valkiria
-	ln -s $PWD/Godeps $GOPATH/src/github.com/Stratio/valkiria/Godeps
-	ln -s $PWD/dbus $GOPATH/src/github.com/Stratio/valkiria/dbus
-	ln -s $PWD/routes $GOPATH/src/github.com/Stratio/valkiria/routes
-	ln -s $PWD/proc $GOPATH/src/github.com/Stratio/valkiria/proc
-	ln -s $PWD/vendor $GOPATH/src/github.com/Stratio/valkiria/vendor
-	ln -s $PWD/test $GOPATH/src/github.com/Stratio/valkiria/test
-fi
+echo $PWD ---- $GOPATH
+	[ -d $GOPATH/src/github.com/Stratio/valkiria/valkiria ] || ln -s $PWD/valkiria $GOPATH/src/github.com/Stratio/valkiria/valkiria
+	[ -d $GOPATH/src/github.com/Stratio/valkiria/Godeps ] 	|| ln -s $PWD/Godeps $GOPATH/src/github.com/Stratio/valkiria/Godeps
+	[ -d $GOPATH/src/github.com/Stratio/valkiria/dbus ] 	|| ln -s $PWD/dbus $GOPATH/src/github.com/Stratio/valkiria/dbus
+	[ -d $GOPATH/src/github.com/Stratio/valkiria/routes ] 	|| ln -s $PWD/routes $GOPATH/src/github.com/Stratio/valkiria/routes
+	[ -d $GOPATH/src/github.com/Stratio/valkiria/proc ] 	|| ln -s $PWD/proc $GOPATH/src/github.com/Stratio/valkiria/proc
+	[ -d $GOPATH/src/github.com/Stratio/valkiria/vendor ] 	|| ln -s $PWD/vendor $GOPATH/src/github.com/Stratio/valkiria/vendor
+	[ -d $GOPATH/src/github.com/Stratio/valkiria/test ] 	|| ln -s $PWD/test $GOPATH/src/github.com/Stratio/valkiria/test
 cd $GOPATH/src/github.com/Stratio/valkiria
 $GOPATH/bin/godep go install -v -ldflags "-w $LDFLAGS" $ALLPACKAGE
