3f346d6aea5527b8c9241060b3a42cdb7d411322
jpgilaberte <jpgilaberte@stratio.com>
2017-01-18 15:03:27 +0100

    Fix link compile.sh

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,7 +2,7 @@
 
 . bin/commons.sh
 
-ALLPACKAGE="./..."
+ALLPACKAGE="./dbus ./proc ./valkiria ./manager ./plugin ./workers ./routes"
 LOGLEVEL=INFO
 BUILD=$(git rev-parse HEAD)
 LDFLAGS="-X main.Version=$VERSION -X main.Build=$BUILD -X main.LOGLEVEL=$LOGLEVEL"
@@ -18,8 +18,8 @@ go get -u github.com/jstemmer/go-junit-report
 [ -d $GOPATH/src/github.com/Stratio/valkiria/proc ] 	|| ln -s $PWD/proc $GOPATH/src/github.com/Stratio/valkiria/proc
 [ -d $GOPATH/src/github.com/Stratio/valkiria/vendor ] 	|| ln -s $PWD/vendor $GOPATH/src/github.com/Stratio/valkiria/vendor
 [ -d $GOPATH/src/github.com/Stratio/valkiria/test ] 	|| ln -s $PWD/test $GOPATH/src/github.com/Stratio/valkiria/test
-[ -d $GOPATH/src/github.com/Stratio/valkiria/manager ] 	|| ln -s $PWD/test $GOPATH/src/github.com/Stratio/valkiria/manager
-[ -d $GOPATH/src/github.com/Stratio/valkiria/plugin ] 	|| ln -s $PWD/test $GOPATH/src/github.com/Stratio/valkiria/plugin
-[ -d $GOPATH/src/github.com/Stratio/valkiria/workers ] 	|| ln -s $PWD/test $GOPATH/src/github.com/Stratio/valkiria/workers
+[ -d $GOPATH/src/github.com/Stratio/valkiria/manager ] 	|| ln -s $PWD/manager $GOPATH/src/github.com/Stratio/valkiria/manager
+[ -d $GOPATH/src/github.com/Stratio/valkiria/plugin ] 	|| ln -s $PWD/plugin $GOPATH/src/github.com/Stratio/valkiria/plugin
+[ -d $GOPATH/src/github.com/Stratio/valkiria/workers ] 	|| ln -s $PWD/workers $GOPATH/src/github.com/Stratio/valkiria/workers
 cd $GOPATH/src/github.com/Stratio/valkiria
-$GOPATH/bin/godep go install -v -ldflags "-w $LDFLAGS" $ALLPACKAGE
+$GOPATH/bin/godep go install -v $ALLPACKAGE
