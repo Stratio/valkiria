d14134d804accb33a179afc6764ad8d99297245d
Antonio Alfonso Pujante <aalfonso@stratio.com>
2017-01-19 15:53:16 +0100

    fix coverage analysis (#21)

--- bin/cover.sh
+++ bin/cover.sh
@@ -9,9 +9,9 @@ if [ -d "$GOPATH" ]; then
     cd $GOPATH/src/github.com/Stratio/valkiria
     $GOPATH/bin/godep go test -v -coverprofile=coverageDbus.out ./dbus | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportDbus.xml
     $GOPATH/bin/godep go test -v -coverprofile=coverageProc.out ./proc | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportProc.xml
-    $GOPATH/bin/godep go test -v -coverprofile=coveragePlugin.out ./proc | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportPlugin.xml
-    $GOPATH/bin/godep go test -v -coverprofile=coverageManager.out ./proc | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportManager.xml
-    $GOPATH/bin/godep go test -v -coverprofile=coverageWorkers.out ./proc | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportWorkers.xml
+    $GOPATH/bin/godep go test -v -coverprofile=coveragePluginMesos.out ./plugin/mesos | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportPluginMesos.xml
+    $GOPATH/bin/godep go test -v -coverprofile=coverageManager.out ./manager | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportManager.xml
+    $GOPATH/bin/godep go test -v -coverprofile=coverageWorkers.out ./workers | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportWorkers.xml
 #    $GOPATH/bin/godep go test -v -coverprofile=coverageRoutes.out ./routes | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportRoutes.xml
 #    $GOPATH/bin/godep go test -v -coverprofile=coverageValkiria.out ./valkiria | $GOPATH/bin/go-junit-report > $SUREFIRE_REPORTS_PATH/reportValkiria.xml
 
@@ -19,7 +19,7 @@ if [ -d "$GOPATH" ]; then
     echo "mode: set" > coverageAll.out
     cat coverageDbus.out | grep -v "mode: set" >> coverageAll.out
     cat coverageProc.out | grep -v "mode: set" >> coverageAll.out
-    cat coveragePlugin.out | grep -v "mode: set" >> coverageAll.out
+    cat coveragePluginMesos.out | grep -v "mode: set" >> coverageAll.out
     cat coverageManager.out | grep -v "mode: set" >> coverageAll.out
     cat coverageWorkers.out | grep -v "mode: set" >> coverageAll.out
 #    cat coverageRoutes.out | grep -v "mode: set" >> coverageAll.out
