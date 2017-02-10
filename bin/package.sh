c34ec65ead01111926a04d9107e5909578e11d77
Emilio Ambrosio Cordero <eambrosio@stratio.com>
2017-02-10 11:16:22 +0100

    change valkiria binary path within tar.gz (#23)

--- bin/package.sh
+++ bin/package.sh
@@ -3,7 +3,7 @@
 . bin/commons.sh
 
 if [ -d "$GOPATH" ]; then
-    tar -zcvf $GOPATH/bin/valkiria-${VERSION}.tar.gz $GOPATH/bin/valkiria
+    tar -cvf $GOPATH/bin/valkiria-${VERSION}.tar.gz -C $GOPATH/bin valkiria
 else
     echo "target file not available, please run 'make compile' first"
 fi
