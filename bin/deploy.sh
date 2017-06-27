6be6a9910fe726bcd99e70a4a8e7e2b19a145848
Antonio Alfonso Pujante <aalfonso@stratio.com>
2017-06-27 09:01:09 +0200

    update path in nexus

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,7 @@
 . bin/commons.sh
 
 if [ -d "$GOPATH" ]; then
-    cd ${GOPATH}/bin && curl -sS -u stratio:${NEXUSPASS} --upload-file valkiria-${VERSION}.tar.gz http://sodio.stratio.com/nexus/content/sites/paas/valkiria/${VERSION}/
+    cd ${GOPATH}/bin && curl -sS -u stratio:${NEXUSPASS} --upload-file valkiria-${VERSION}.tar.gz http://sodio.stratio.com/repository/paas/valkiria/${VERSION}/
 else
     echo "target file not available, please run 'make compile' first"
 fi
